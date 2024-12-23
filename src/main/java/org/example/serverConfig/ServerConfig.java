b5ab232a9f76db621c4572536e3a5f8676a13987
Bratsk007 <matveipak@bk.ru>
2024-12-23 16:40:00 +0300

    added username and password validation

--- src/main/java/org/example/serverConfig/ServerConfig.java
+++ src/main/java/org/example/serverConfig/ServerConfig.java
@@ -4,7 +4,7 @@ package org.example.serverConfig;
  * Класс с данными для подключения к серверу
  */
 public enum ServerConfig {
-    SERVER_ADDRESS("192.168.185.170"),
+    SERVER_ADDRESS("192.168.1.49"),
     SERVER_PORT("8082");
 
     private String value;
